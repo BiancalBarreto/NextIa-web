beb1be8afff41dabf8be154e9dd6b92f79a98c8b
Bianca Leandro Barreto <72078345+BiancalBarreto@users.noreply.github.com>
2024-05-20 17:33:36 -0300

    Add files via upload

---
+++ routes/tab.routes.tsx
@@ -0,0 +1,15 @@
+
+// import Pontos from '@/screens/pontos';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+ 
+
+// const Tab = createBottomTabNavigator();
+
+
+// export default function TabRoutes(){
+//     return(
+//         <Tab.Navigator >
+//             <Tab.Screen name='pontos' component={ Pontos }/>
+//         </Tab.Navigator>
+//     )
+// }
